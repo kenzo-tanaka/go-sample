62fa0893898d52092f1c187eca9fdf7f1e01c358
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-01-04 20:10:12 +0900

    structを使った簡単なコードで実装

--- sample_test.go
+++ sample_test.go
@@ -6,10 +6,22 @@ import (
 	"./calc"
 )
 
+type Person struct {
+	Lastname string
+	Firstname string
+}
+
 func add(a, b int) int {
 	return a + b
 }
 
 func TestAdd(t *testing.T) {
 	assert.Equal(t, calc.Add(1,1), 2)
+}
+
+func TestPerson(t *testing.T) {
+	person := new(Person)
+	person.Lastname = "Kenzo"
+	person.Firstname = "Tanaka"
+	assert.Equal(t, person.Lastname, "Kenzo")
 }
